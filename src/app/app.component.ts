ebf67a75e4d92ee5ea54dba6628c2c6ac7a67a04
davidm1806 <menfah2dev@gmail.com>
2020-06-23 17:07:27 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit{
+  formGroup: FormGroup;
+  errorMessage: string;
+  authentification = [
+    {username: 'user', password: '123456'},
+    {username: 'admin', password: 'admin'}
+  ];
+  constructor(private formBuilder: FormBuilder, private snarBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  singIn() {
+    if (this.formGroup.invalid) {
+      this.snarBar.open('le formulaire est incorrect', 'x', {
+        panelClass: 'panel-error',
+        duration: 5000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top'
+      });
+      return;
+    }
+    this.errorMessage = '';
+    let goodPassword: boolean;
+    let goodUsername: boolean;
+    this.authentification.forEach(auth => {
+      if(auth.username == this.formGroup.controls.username.value)
+        goodUsername = true;
+      if(auth.password == this.formGroup.controls.password.value)
+        goodPassword = true;
+    });
+    if (goodPassword && goodUsername) {
+      this.errorMessage = '';
+      this.snarBar.open('Bravooooo!!!! vous vous êtes identifier avec succes', 'x', {
+        panelClass: 'panel-success',
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+    } else {
+      if(goodUsername)
+        this.errorMessage = 'Votre mot de pass est incorect';
+      else
+        this.errorMessage = 'ce compte n\'existe pas';
+    }
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  ErrorStateMatcher,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatSnackBarModule,
+  ShowOnDirtyErrorStateMatcher
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
